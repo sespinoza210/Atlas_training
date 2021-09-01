b5b282fb4c2fc6de823e5cd123945e38658cee1e
Sergio Espinoza <sergio.espinoza@wpengine.com>
2021-09-01 18:49:22 -0500

    404 update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,13 +13,13 @@ export default function Page(): JSX.Element {
         description={generalSettings?.description}
       />
       <main className="content content-page">
-        <Hero title={`Oops! That page can’t be found.`} />
+        <Hero title={`Oops! That page can’t be found. Try some other time maybe?`} />
         <div className="wrap">
           <div>
             <div>
               <p>
                 The page you were looking for does not exist or is no longer
-                available.
+                available due to death.
               </p>
             </div>
           </div>
